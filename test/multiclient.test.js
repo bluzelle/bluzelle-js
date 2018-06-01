4068cb2406ef4979049a5d10cd3374b314a16376
jyclam <johnlamyc@gamil.com>
2018-06-01 16:46:04 -0700

    Refactor multiclient tests for clarity

--- test/multiclient.test.js
+++ test/multiclient.test.js
@@ -13,262 +13,269 @@ const api2 = require('../api');
 
 
 // Run if not testing in browser
-(typeof window === 'undefined' ? describe : describe.skip)('multi-client bluzelle api', () => {
+(typeof window === 'undefined' ? describe.only : describe.skip)('multi-client bluzelle api', () => {
 
     beforeEach(reset);
 
     process.env.daemonIntegration && afterEach(killSwarm);
 
-    describe('two clients with different UUID\'s interacting with the same key', () => {
+    describe('two clients with different UUID\'s', () => {
 
-        beforeEach(() => {
-            api1.connect(`ws://${process.env.address}:${process.env.port}`, '4982e0b0-0b2f-4c3a-b39f-26878e2ac814');
+        context('interacting with the same key', () => {
 
-            api2.connect(`ws://${process.env.address}:${process.env.port}`, '71e2cd35-b606-41e6-bb08-f20de30df76c');
-        });
+            beforeEach(() => {
+                api1.connect(`ws://${process.env.address}:${process.env.port}`, '4982e0b0-0b2f-4c3a-b39f-26878e2ac814');
 
-        // it('api1 should be able to ping the connection', () =>
-        //     api1.ping());
+                api2.connect(`ws://${process.env.address}:${process.env.port}`, '71e2cd35-b606-41e6-bb08-f20de30df76c');
+            });
 
-        // it('api2 should be able to ping the connection', () =>
-        //     api2.ping());
+            // it('api1 should be able to ping the connection', () =>
+            //     api1.ping());
 
-        it('api1 should be able to write to database', async () => {
-            await api1.create('myKey', 123);
-            assert(await api1.read('myKey') === 123);
-        });
+            // it('api2 should be able to ping the connection', () =>
+            //     api2.ping());
 
-        it('api2 should be able to write to database', async () => {
-            await api2.create('myKey', 345);
-            assert(await api2.read('myKey') === 345);
-        });
-
-        describe('number fields', async () => {
-
-            beforeEach(async () => {
+            it('client1 should be able to write to database', async () => {
                 await api1.create('myKey', 123);
-                await api2.create('myKey', 345);
-            });
-
-            it('should be able to read with no collision', async () => {
                 assert(await api1.read('myKey') === 123);
+            });
+
+            it('client2 should be able to write to database', async () => {
+                await api2.create('myKey', 345);
                 assert(await api2.read('myKey') === 345);
             });
 
-            it('should be able to update from one and not affect the other', async () => {
-                await api1.update('myKey', 999);
+            context('number fields', async () => {
 
-                assert(await api2.read('myKey') === 345);
-            });
+                beforeEach(async () => {
+                    await api1.create('myKey', 123);
+                    await api2.create('myKey', 345);
+                });
 
-            it('should be able to delete from one and not affect the other', async () => {
-                await api1.remove('myKey');
+                it('should be able to read with no cross talk', async () => {
+                    assert(await api1.read('myKey') === 123);
+                    assert(await api2.read('myKey') === 345);
+                });
 
-                assert(await api2.read('myKey') === 345);
-            });
+                it('should be able to update with no cross talk', async () => {
+                    await api1.update('myKey', 999);
 
-        });
+                    assert(await api2.read('myKey') === 345);
+                });
 
-        describe('text fields', async () => {
+                it('should be able to delete with no cross talk', async () => {
+                    await api1.remove('myKey');
 
-            beforeEach(async () => {
-                await api1.create('myKey', 'hello world');
-                await api2.create('myKey', 'good morning');
-            });
-
-            it('should be able to read with no collision', async () => {
-                assert(await api1.read('myKey') === 'hello world');
-                assert(await api2.read('myKey') === 'good morning');
-
-            });
-
-            it('should be able to update from one and not affect the other', async () => {
-                await api1.update('myKey', 'changed value');
-
-                assert(await api2.read('myKey') === 'good morning');
-            });
-
-            it('should be able to delete from one and not affect the other', async () => {
-                await api1.remove('myKey');
-
-                assert(await api2.read('myKey') === 'good morning');
-            });
-
-        });
-
-        describe('object fields', async () => {
-
-            beforeEach(async () => {
-                await api1.create('myKey', {a: 5});
-                await api2.create('myKey', {b: 9});
-            });
-
-            it('should be able to read with no collision', async () => {
-                assert((await api1.read('myKey')).a === 5);
-                assert((await api2.read('myKey')).b === 9);
+                    assert(await api2.read('myKey') === 345);
+                });
 
             });
 
-            it('should be able to update from one and not affect the other', async () => {
-                await api1.update('myKey', {a: 500});
+            context('text fields', async () => {
 
-                assert((await api2.read('myKey')).b === 9);
+                beforeEach(async () => {
+                    await api1.create('myKey', 'hello world');
+                    await api2.create('myKey', 'good morning');
+                });
+
+                it('should be able to read with no cross talk', async () => {
+                    assert(await api1.read('myKey') === 'hello world');
+                    assert(await api2.read('myKey') === 'good morning');
+
+                });
+
+                it('should be able to update with no cross talk', async () => {
+                    await api1.update('myKey', 'changed value');
+
+                    assert(await api2.read('myKey') === 'good morning');
+                });
+
+                it('should be able to delete with no cross talk', async () => {
+                    await api1.remove('myKey');
+
+                    assert(await api2.read('myKey') === 'good morning');
+                });
+
             });
 
-            it('should be able to delete from one and not affect the other', async () => {
-                await api1.remove('myKey');
+            context('object fields', async () => {
 
-                assert((await api2.read('myKey')).b === 9);
+                beforeEach(async () => {
+                    await api1.create('myKey', {a: 5});
+                    await api2.create('myKey', {b: 9});
+                });
+
+                it('should be able to read with no cross talk', async () => {
+                    assert((await api1.read('myKey')).a === 5);
+                    assert((await api2.read('myKey')).b === 9);
+
+                });
+
+                it('should be able to update with no cross talk', async () => {
+                    await api1.update('myKey', {a: 500});
+
+                    assert((await api2.read('myKey')).b === 9);
+                });
+
+                it('should be able to delete with no cross talk', async () => {
+                    await api1.remove('myKey');
+
+                    assert((await api2.read('myKey')).b === 9);
+                });
+
             });
 
-        });
+            describe('attempting to access keys of another client', () => {
 
-        describe('attempting to access keys of another client', () => {
+                beforeEach(async () => {
+                    await api1.create('onlyInOne', 'something');
+                });
 
-            beforeEach(async () => {
-                await api1.create('onlyInOne', 'something');
+                it('should only be able to access keys in its database', async () => {
+                    assert(await api1.has('onlyInOne'));
+                    assert(!await api2.has('onlyInOne'));
+                });
+
+                context('should throw an error', () => {
+
+                    it('when trying to read a key not in its database', done => {
+                        api2.read('onlyInOne').catch(() => done());
+                    });
+
+                    it('when trying to update a key not in its database', done => {
+                        api2.update('onlyInOne', '123').catch(() => done());
+                    });
+
+                    it('when trying to delete a key not in its database', done => {
+                        api2.remove('onlyInOne').catch(() => done());
+                    });
+                });
             });
-
-            it('should only be able to access keys in its database', async () => {
-                assert(await api1.has('onlyInOne'));
-                assert(!await api2.has('onlyInOne'));
-            });
-
-            it('should throw an error when trying to read a key not in its database', done => {
-                api2.read('onlyInOne').catch(() => done());
-            });
-
-            it('should throw an error when trying to update a key not in its database', done => {
-                api2.update('onlyInOne', '123').catch(() => done());
-            });
-
-            it('should throw an error when trying to delete a key not in its database', done => {
-                api2.remove('onlyInOne').catch(() => done());
-            });
-
         });
     });
 
-    describe('two clients with the same UUID\'s interacting with the same key', () => {
+    describe('two clients with the same UUID\'s', () => {
 
-        beforeEach(() => {
-            api1.connect(`ws://${process.env.address}:${process.env.port}`, '4982e0b0-0b2f-4c3a-b39f-26878e2ac814');
+        context('interacting with the same key', () => {
 
-            api2.connect(`ws://${process.env.address}:${process.env.port}`, '4982e0b0-0b2f-4c3a-b39f-26878e2ac814');
-        });
+            beforeEach(() => {
+                api1.connect(`ws://${process.env.address}:${process.env.port}`, '4982e0b0-0b2f-4c3a-b39f-26878e2ac814');
 
-        // it('api1 should be able to ping the connection', () =>
-        //     api1.ping());
+                api2.connect(`ws://${process.env.address}:${process.env.port}`, '4982e0b0-0b2f-4c3a-b39f-26878e2ac814');
+            });
 
-        // it('api2 should be able to ping the connection', () =>
-        //     api2.ping());
+            // it('api1 should be able to ping the connection', () =>
+            //     api1.ping());
 
-        it('api1 should be able to write to database', async () => {
-            await api1.create('myKey', 123);
-            assert(await api1.read('myKey') === 123);
-        });
+            // it('api2 should be able to ping the connection', () =>
+            //     api2.ping());
 
-        it('api2 should be able to write to database', async () => {
-            await api2.create('myKey', 345);
-            assert(await api2.read('myKey') === 345);
-        });
+            it('client1 should be able to write to database', async () => {
+                await api1.create('myKey', 123);
+                assert(await api1.read('myKey') === 123);
+            });
 
-        it('should throw an error when creating the same key twice', done => {
+            it('client2 should be able to write to database', async () => {
+                await api2.create('myKey', 345);
+                assert(await api2.read('myKey') === 345);
+            });
 
-            api1.create('mykey', 123).then(() => {
+            it('should throw an error when creating the same key twice', done => {
 
-                api2.create('mykey', 321).catch(() => done());
+                api1.create('mykey', 123).then(() => {
+
+                    api2.create('mykey', 321).catch(() => done());
+
+                });
 
             });
 
-        });
 
-        describe('api1\'s key should be mutated by api2\'s call', async () => {
+            context('creating, updating, and then reading', () => {
 
-            describe('creating, updating, and then reading', () => {
-
-                describe('number fields', () => {
+                context('number fields', () => {
 
                     beforeEach(async () => {
                         await api1.create('myNumKey', 123);
                         await api2.update('myNumKey', 345);
                     });
 
-                    it('value should be updated by api2', async () => {
+                    it('value should be updated by last call', async () => {
                         assert(await api1.read('myNumKey') !== 123);
                         assert(await api1.read('myNumKey') === 345);
                     });
                 });
 
-                describe('text fields', () => {
+                context('text fields', () => {
 
                     beforeEach(async () => {
                         await api1.create('myTextKey', 'hello world');
                         await api2.update('myTextKey', 'goodbye world');
                     });
 
-                    it('value should be updated by api2', async () => {
+                    it('value should be updated by last call', async () => {
                         assert(await api1.read('myTextKey') !== 'hello world');
                         assert(await api1.read('myTextKey') === 'goodbye world');
                     });
                 });
 
-                describe('object fields', () => {
+                context('object fields', () => {
 
                     beforeEach(async () => {
-                        await api1.create('myObjKey', { a : 5 });
-                        await api2.update('myObjKey', { a : 100});
+                        await api1.create('myObjKey', {a: 5});
+                        await api2.update('myObjKey', {a: 100});
                     });
 
-                    it('value should be updated by api2', async () => {
-                        assert((await api1.read('myObjKey')).a !== 5 );
+                    it('value should be updated by last call', async () => {
+                        assert((await api1.read('myObjKey')).a !== 5);
                         assert((await api1.read('myObjKey')).a === 100);
                     });
                 });
 
             });
 
-            describe('creating, deleting, and then reading', () => {
+            context('creating, deleting, and then reading', () => {
 
-                describe('number field', () => {
+                context('number field', () => {
 
                     beforeEach(async () => {
                         await api1.create('myNumKey', 123);
                         await api2.remove('myNumKey');
                     });
 
-                    it('throw error when attempting to read', done => {
+                    it('should throw error when attempting to read', done => {
                         api1.read('myNumKey').catch(() => done());
                     });
 
                 });
 
-                describe('text field', () => {
+                context('text field', () => {
 
                     beforeEach(async () => {
                         await api1.create('myTextKey', 'hello world');
                         await api2.remove('myTextKey');
                     });
 
-                    it('throw error when attempting to read', done => {
+                    it('should throw error when attempting to read', done => {
                         api1.read('myTextKey').catch(() => done());
                     });
 
                 });
 
-                describe('object field', () => {
+                context('object field', () => {
 
                     beforeEach(async () => {
-                        await api1.create('myObjKey', { a : 5 });
+                        await api1.create('myObjKey', {a: 5});
                         await api2.remove('myObjKey');
                     });
 
-                    it('throw error when attempting to read', done => {
+                    it('should throw error when attempting to read', done => {
                         api1.read('myObjKey').catch(() => done());
                     });
 
                 });
             });
         });
+
     });
 });
