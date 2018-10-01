a1baae8bea6feabf6fdd94fe047b373735690682
Monty Thibault <monty@bluzelle.com>
2018-10-01 15:26:16 -0400

    pbft double response

--- src/api.js
+++ src/api.js
@@ -27,6 +27,11 @@ const newConnection = (client, address, connectionObject = {}) =>
 });
 
 
+
+const ackMessage = response => 
+        response.getResponseCase() === 0;
+
+
 const onMessage = (client, bin) => {    
 
     if(typeof bin === 'string') {
@@ -56,9 +61,26 @@ const onMessage = (client, bin) => {
         throw new Error('Bluzelle: (internal) received non-response_json message.');
     }
 
+    // If the message is not in the resolution map, it has already
+    // been resolved from the acknowledgement.
+
+    if(!client.tidMap.has(id)) {
+        return;
+    }
+
 
     const o = client.tidMap.get(id);
-    client.tidMap.delete(o);
+
+
+
+    // Delete the message from the resolution map it's not needed
+    // after this.
+
+    if(o.resolve_on_ack || !ackMessage(response)) {
+
+        tidMap.delete(o);
+
+    }
 
 
     if(response_json.redirect) {
@@ -84,7 +106,12 @@ const onMessage = (client, bin) => {
 
         } else {
 
-            resolve(response_json, response, o);
+            if((ackMessage(response) && o.resolve_on_ack) ||
+               (!ackMessage(response) && !o.resolve_on_ack)) {
+
+                resolve(response_json, response, o);
+
+            }       
 
         }
 
@@ -118,7 +145,7 @@ const resolve = (response_json, response, o) => {
 };
 
 
-const send = (client, database_msg, socket) => 
+const send = (client, database_msg, socket, resolve_on_ack) => 
 
     new Promise((resolve, reject) => {
 
@@ -147,6 +174,7 @@ const send = (client, database_msg, socket) =>
 
 
     client.tidMap.set(tid, {
+        resolve_on_ack,
         resolve,
         reject,
         database_msg
@@ -160,7 +188,7 @@ const send = (client, database_msg, socket) =>
 });
 
 
-const sendPrimary = (client, database_msg) => {
+const sendPrimary = (client, database_msg, resolveOnAck=false) => {
 
     if(!client.primaryConnection.socket) {
 
@@ -169,7 +197,7 @@ const sendPrimary = (client, database_msg) => {
 
     }
 
-    return send(client, database_msg, client.primaryConnection.socket);
+    return send(client, database_msg, client.primaryConnection.socket, resolveOnAck);
 
 };
 
@@ -177,46 +205,17 @@ const sendPrimary = (client, database_msg) => {
 // Attempts to send to the secondary connection and otherwise
 // defaults to the primary.
 
-const sendSecondary = (client, database_msg) => {
+const sendSecondary = (client, database_msg, resolveOnAck=false) => {
 
     if(client.secondaryConnection.socket) {
 
-        return send(client, database_msg, client.secondaryConnection.socket);
+        return send(client, database_msg, client.secondaryConnection.socket, resolveOnAck);
 
     } else {
 
-        return sendPrimary(client, database_msg);
+        return sendPrimary(client, database_msg, resolveOnAck);
 
     }
-
-
-};
-
-
-const sendObserver = (client, database_msg, observer) => {
-
-    // This function replaces the resolver and then calls the observer,
-    // as the resolver is automatically deleted after it resolves.
-
-    const persistResolver = v => {
-
-        const tid = database_msg.getHeader().getTransactionId();
-
-        client.tidMap.set(tid, {
-            resolve: persistResolver,
-            reject: () => {},
-            db_message: {}
-        });
-
-
-        v.subscriptionUpdate && observer(v.subscriptionUpdate.value);
-
-        return tid;
-
-    };
-
-
-    return sendPrimary(client, database_msg).then(persistResolver);
 
 };
 
@@ -260,7 +259,6 @@ class BluzelleClient {
 
         this.verbose = verbose;
 
-
         this.primaryConnection = {};
         this.secondaryConnection = {};
 
@@ -290,7 +288,6 @@ class BluzelleClient {
             return Promise.reject(new Error('bluzelle: already connected.'));
 
         }
-
 
         return newConnection(this, this.entrypoint, this.primaryConnection);
 
@@ -354,7 +351,7 @@ class BluzelleClient {
         database_msg.setKeys(database_empty);
 
 
-        return sendPrimary(this, database_msg).then(o => o.keys.keysList);
+        return sendPrimary(this, database_msg, false).then(o => o.keys.keysList);
 
     }
 
@@ -389,7 +386,7 @@ class BluzelleClient {
         database_msg.setUpdate(database_update);
 
 
-        return sendSecondary(this, database_msg);
+        return sendSecondary(this, database_msg, true);
 
     }
 
@@ -407,7 +404,7 @@ class BluzelleClient {
         database_msg.setCreate(database_create);
 
 
-        return sendSecondary(this, database_msg);
+        return sendSecondary(this, database_msg, true);
 
     }
 
@@ -423,97 +420,60 @@ class BluzelleClient {
         database_msg.setDelete(database_delete);
 
 
+        return sendSecondary(this, database_msg, true);
+
+    }
+
+
+
+    update(key, value) {
+
+        const database_msg = getMessagePrototype(this);
+
+        const database_update = new bluzelle_pb.database_update();
+
+        database_update.setKey(key);
+
+        database_update.setValue(valToUInt8(value));
+
+        database_msg.setUpdate(database_update);
+
+
         return sendSecondary(this, database_msg);
 
     }
 
 
-
-    // Subscription
-
-
-    subscribe(key, observer) {
-
-        const database_msg = getMessagePrototype(this);
-
-        const database_subscribe = new bluzelle_pb.database_subscribe();
-
-        database_subscribe.setKey(key);
-
-        database_msg.setSubscribe(database_subscribe);
-
-
-        this.tidToKey.set(
-            database_msg.getHeader().getTransactionId(),
-            key);
-
-
-        return sendObserver(this, database_msg, v => observer(uInt8ToVal(v)));
-
-    }
-
-
-    unsubscribe(tid) {
-
-        const database_msg = getMessagePrototype(this);
-
-        const database_unsubscribe = new bluzelle_pb.database_unsubscribe();
-
-
-        const key = this.tidToKey.get(tid);
-
-
-        database_unsubscribe.setKey(key);
-        database_unsubscribe.setTransactionId(tid);
-
-        database_msg.setUnsubscribe(database_unsubscribe);
-
-
-        return sendPrimary(this, database_msg).then(() => 
-
-            this.tidToKey.delete(tid));
-
-    };
-
-
-    async _subscriptionAction(key, value, action) {
-
-        let v;
-        let set;
-
-        const s = await this.subscribe(key, v2 => { set = true; v = v2; });
-
-        await action();
-
-        await waitUntil(() => set === true && v === value);
-
-        await this.unsubscribe(s);
-
-    }
-
-
-    // Composite functions
-
     create(key, value) {
 
-        return this._subscriptionAction(key, value, 
-            () => this.createAck(key, value));
+        const database_msg = getMessagePrototype(this);
+
+        const database_create = new bluzelle_pb.database_create();
+
+        database_create.setKey(key);
+
+        database_create.setValue(valToUInt8(value));
+
+        database_msg.setCreate(database_create);
+
+
+        return sendSecondary(this, database_msg);
 
     }
 
 
-    update(key, value) {
+    remove(key) {
 
-        return this._subscriptionAction(key, value,
-            () => this.updateAck(key, value));
+        const database_msg = getMessagePrototype(this);
 
-    }
+        const database_delete = new bluzelle_pb.database_delete();
+
+        database_delete.setKey(key);
+
+        database_msg.setDelete(database_delete);
 
 
-    remove(key) { 
-
-        return this._subscriptionAction(key, undefined,
-            () => this.removeAck(key));
+        return sendSecondary(this, database_msg);
 
     }
 
@@ -522,7 +482,5 @@ class BluzelleClient {
 
 
 module.exports = {
-
     BluzelleClient
-
 };
