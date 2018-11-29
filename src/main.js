7635ec1613523019b1d815a7961b18a9a98ca7ae
Monty Thibault <monty@bluzelle.com>
2018-11-29 14:22:03 -0700

    Added license to js files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+// Copyright (C) 2018 Bluzelle
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License, version 3,
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 const Connection = require('./1_connection_layer');
 const Crypto = require('./2_crypto_layer');
 const Redirect = require('./3_redirect_layer');
