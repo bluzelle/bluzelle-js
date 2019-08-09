c6fe31c67d14daf3c00ebe0e01dee3eb567dcc90
Monty Thibault <monty@bluzelle.com>
2019-08-09 10:37:59 -0600

    Updated crypto test file name

--- test-browser/main.test.js
+++ test-browser/main.test.js
@@ -2,5 +2,5 @@
 
 // It is webpack'd into main.test.pack.js and included in mocha.html.
 
-require('../src/swarmClient/ecdsa_secp256k1.test.js');
+require('../src/swarmClient/ecdsa_p256.test.js');
 require('../src/test/main.test.js');
